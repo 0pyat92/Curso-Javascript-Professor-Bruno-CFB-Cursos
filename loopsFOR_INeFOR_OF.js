a7e1e36e2ca8f9e2f33db4234d0ae18eb4306512
0pripyat92 <devjr0000@gmail.com>
2024-01-31 21:53:41 -0300

    commit aula 16

---
+++ loopsFOR_INeFOR_OF.js
@@ -0,0 +1,15 @@
+const objs=document.getElementsByTagName("div")
+
+let num=[10,20,30,40,50]
+
+for(o of objs){
+    console.log(o)
+}
+
+for(n in num){
+    console.log(n)
+}
+
+/*for(let i=0;i<num.length;i++){
+    console.log(num[i])
+}*/
